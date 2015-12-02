2a10ec8c46b7edaad6e3d09531b947e21ad2bf2e
Clyde D'Souza <clyd94@gmail.com>
2015-12-02 22:51:00 +1300

    added index for heroku
    u

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+	<!----responsive----->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!---meta---->
+<meta name="Keywords" content=""/> 
+<meta name="author" content="">
+<meta name="description" content="">
+<meta itemprop="image" content="" />
+<meta name="og_site_name" property="og:site_name" content=""/> 
+<meta name="og_url" property="og:url" content=""/>
+	<!----favicon------->
+<title>A1 Professional Designs</title>
+	<!-- CSS -->
+<link href="assets/css/bootstrap.css" rel="stylesheet" />	
+<link href="assets/css/main.css" rel="stylesheet" />
+<link rel="stylesheet" href="assets/css/font-awesome.min.css" />   
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="assets/js/html5shiv.js"></script>
+<script src="assets/js/respond.min.js"></script>
+<![endif]-->
+</head>
+<body data-spy="scroll" data-offset="0" data-target="#theMenu" onLoad="loadHome('home')">		
+<!-- ========== MENU SECTION ========== -->
+<div class="hidden-md hidden-lg visible-sm visible-xs">
+	<div>
+		<span id="menuToggle">
+			<span class="icon-reorder"></span>
+		</span>
+	</div>
+	<div id="mobileMenu">
+	</div>
+</div>
+<!-- ========== MENU SECTION ========== -->
+<!-- ========== HEADER SECTION ========== -->
+<div id="headerSection">
+</div>
+<!-- ========== TEXT SECTION ========== -->
+<div class="container">
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center tag-line">
+			<h1 class="jumbotron-heading">A1 Professsional Designs</h1>
+			<p class="lead">Do you need to get yourself a business card? Or do you need to print and drop in flyers of your new business? Don't worry. At A1 Professional Designs we cater to all your print needs. Right from business cards to flyers, birthday invitation cards to specialised greeting cards, we do all of this (and more) at the best possible price. Get in touch now!</p>
+			<p><span class="btn btn-default">Roshini Daruwala 021154876</span></p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-6 col-md-4 col-lg-4 col-xs-12">
+			<div class="thumbnail">
+			<img src="assets/img/gray.png" alt="" class="img-responsive">
+			<div class="caption">
+			<h2>Business Cards</h2>
+			<p>Go pro with business cards for yourself or your business</p>
+			<p><a href="#" class="btn btn-default" role="button">More</a></p>
+			</div>
+			</div>
+		</div>
+		<div class="col-sm-6 col-md-4 col-lg-4 col-xs-12">
+			<div class="thumbnail">
+			<img src="assets/img/pink.png" alt="...">
+			<div class="caption">
+			<h2>Letterheads</h2>
+			<p>Make a statement with amazing letterhead designs</p>
+			<p><a href="#" class="btn btn-default" role="button">More</a></p>
+			</div>
+			</div>
+		</div>
+			<div class="col-sm-6 col-md-4 col-lg-4 col-xs-12">
+			<div class="thumbnail">
+			<img src="assets/img/purple.png" alt="...">
+			<div class="caption">
+			<h2>Flyers</h2>
+			<p>Grab the attention of your audience with promising designs for advt flyers</p>
+			<p><a href="#" class="btn btn-default" role="button">More</a></p>
+			</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-6 col-md-4 col-lg-4 col-xs-12">
+			<div class="thumbnail">
+			<img src="assets/img/yellow.png" alt="...">
+			<div class="caption">
+			<h2>Invitation Cards</h2>
+			<p>Send a warm invite to your loved ones with our own unique touch</p>
+			<p><a href="#" class="btn btn-default" role="button">More</a></p>
+			</div>
+			</div>
+		</div>
+		<div class="col-sm-6 col-md-4 col-lg-4 col-xs-12">
+			<div class="thumbnail">
+			<img src="assets/img/blue.png" alt="...">
+			<div class="caption">
+			<h2>Binding and copy</h2>
+			<p>For the rest of your print, copy and binding needs</p>
+			<p><a href="#" class="btn btn-default" role="button">More</a></p>
+			</div>
+			</div>
+		</div>
+		<div class="col-sm-6 col-md-4 col-lg-4 col-xs-12">
+			<div class="thumbnail">
+			<img src="assets/img/green.png" alt="...">
+			<div class="caption">
+			<h2>Download</h2>
+			<p>View more products in details along with the estimates</p>
+			<p><a href="" class="btn download-btn">Download our Brochure</a></p>
+			</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="row">		
+		<div class="col-lg-12 col-md-12 col-sm-12">
+			<div class="panel panel-default panel-custom">
+				<div class="panel-body text-center">
+				<h2>Get in touch for an obligation-free quote</h2>
+				<p><big><b>Roshini Daruwala</b></big></br>
+				Mobile# 021 XCXCXC | Phone# 09-XVGFTS | roshini.daruwala@gmail.com</br>
+				18C Penruddocke Road, Pakuranga,</br>New Zealand</p>
+				</div>
+			</div>			
+		</div>		
+	</div>
+	<div class="row">
+<div class="color-c"></div><div class="color-m"></div>
+</div>
+</div>
+
+<!--======= Footer ================-->
+<div id="footer"></div>
+
+<!--======= Bootstrap core JavaScript ================-->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/modernizr.custom.js"></script>
+<script src="assets/js/classie.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/smoothscroll.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/contents.js"></script>
+<script src="assets/js/loadFunctions.js"></script>
+
+</body>
+</html>
