322fa2a4dd27ddde8802612129ec434cf2e78f88
Clyde D'Souza <clyd94@gmail.com>
2015-12-12 12:35:54 +1300

    added local copy of SendGrid

--- thanks.php
+++ thanks.php
@@ -1,5 +1,6 @@
 <!-------------Website crafted by Clyde D'Souza  ----------------->
 <?php
+require ('sendgrid-php/sendgrid-php.php');
 $ip = getenv('SENDGRID-USERNAME');
 $api = getenv('SENDGRID-APIKEY');
 $sendgrid = new sendgrid($ip,$api);
