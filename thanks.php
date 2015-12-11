d6c3e1e8c71794347198b747a91dff6b1a5fe0ca
Clyde D'Souza <clyd94@gmail.com>
2015-12-12 11:44:16 +1300

    nope

--- thanks.php
+++ thanks.php
@@ -1,19 +1,19 @@
-<!-------------Website crafted by Clyde D'Souza  http://goo.gl/8yXVaA ----------------->
+<!-------------Website crafted by Clyde D'Souza  ----------------->
 <?php
-require 'vendor/autoload.php';
-$ip = getenv('SENDGRID-USERNAME');
-$api = getenv('SENDGRID-APIKEY');
-$sendgrid = new SendGrid($ip,$api);
-echo $ip;
-$email    = new SendGrid\Email();
+// require 'vendor/autoload.php';
+// $ip = getenv('SENDGRID-USERNAME');
+// $api = getenv('SENDGRID-APIKEY');
+// $sendgrid = new SendGrid($ip,$api);
+// echo $ip;
+// $email    = new SendGrid\Email();
  
-$email->addTo("clyd94@gmail.com")
-      ->setFrom("clyd94@gmail.com")
-      ->setSubject("Sending with SendGrid is Fun")
-      ->setHtml("and easy to do anywhere, even with PHP");
+// $email->addTo("clyd94@gmail.com")
+      // ->setFrom("clyd94@gmail.com")
+      // ->setSubject("Sending with SendGrid is Fun")
+      // ->setHtml("and easy to do anywhere, even with PHP");
  
-$sendgrid->send($email);
-?>
+// $sendgrid->send($email);
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
